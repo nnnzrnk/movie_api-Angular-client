ce1de4b43d92c3b56db4280645bc37333e5f4ef8
Anna Nazarenko <annannazarenko@gmail.com>
2023-12-13 13:25:38 +0100

    update user login component

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -5,6 +5,7 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 // This import displays notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -17,7 +18,8 @@ export class UserLoginFormComponent implements OnInit {
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    private router: Router
   ) {}
 
   ngOnInit(): void {}
@@ -28,7 +30,9 @@ export class UserLoginFormComponent implements OnInit {
       (result) => {
         this.dialogRef.close(); // This will close the modal on success!
         console.log(result);
+        localStorage.setItem('token', result.token)
         this.snackBar.open(result, 'OK', { duration: 2000 });
+        this.router.navigate(['movies']);
       },
       (result) => {
         console.log(result);

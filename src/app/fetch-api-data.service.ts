f1a8090d7ff3d4e179ef504c9f6ddc0c7a3835a0
Anna Nazarenko <annannazarenko@gmail.com>
2023-12-13 13:27:11 +0100

    updated file

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -19,7 +19,6 @@ export class FetchApiDataService {
 
   // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
-    console.log(userDetails);
     return this.http
       .post(apiUrl + 'users', userDetails)
       .pipe(catchError(this.handleError));
@@ -27,7 +26,6 @@ export class FetchApiDataService {
 
   // Making the api call for the user login endpoint
   public userLogin(userDetails: any): Observable<any> {
-    console.log(userDetails);
     return this.http
       .post(apiUrl + 'login', userDetails)
       .pipe(catchError(this.handleError));
